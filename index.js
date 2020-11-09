27f1048abb7aab66e4c0de9ba492d7bfec9c79d6
Mikael Bärlund <midas@iki.fi>
2020-11-09 21:11:39 +0200

    step 3/4/5

--- index.js
+++ index.js
@@ -30,9 +30,48 @@ let persons = [
 app.get('/api/persons', (req, res) => {
     res.json(persons)
 })
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(person => person.id === id)
+    if (person) {
+        response.json(person)
+    } else {
+        response.status(404).end()
+    }
+})
 
 app.get('/info', (req, res) => {
     res.send(`Phonebook has info for ${persons.length} people<br/>${new Date()}`)
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    persons = persons.filter(person => person.id !== id)
+    response.status(204).end()
+})
+
+const generateId = () => {
+    let newId = 1
+        while(persons.map(n => n.id).includes(newId)){
+            newId = Math.floor(Math.random()*100)
+        }
+    return newId
+}
+
+app.post('/api/persons', (request, response) => {
+    const body = request.body
+    if (!body.name) {
+        return response.status(400).json({
+            error: 'name missing'
+        })
+    }
+    const person = {
+        ...body,
+        date: new Date(),
+        id: generateId(),
+    }
+    persons = persons.concat(person)
+    response.json(person)
 })
 
 const PORT = 3001
