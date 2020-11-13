4173be55653636daf571410abfcbecdcbb16e9ba
Mikael Bärlund <midas@iki.fi>
2020-11-13 22:05:51 +0200

    puhelinluettelo step 7-9

--- models/person.js
+++ models/person.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const uniqueValidator = require('mongoose-unique-validator');
 
 const url = process.env.MONGODB_URI
 
@@ -13,18 +14,28 @@ mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFind
     })
 
 const personSchema = new mongoose.Schema({
-    name: String,
-    number: String
+    name: {
+        type: String,
+        minlength: 3,
+        required: true,
+        unique: true
+    },
+    number: {
+        type: String,
+        minlength: 8,
+        required: true
+    }
 })
+personSchema.plugin(uniqueValidator);
 
 const Person = mongoose.model('Person', personSchema)
 
 personSchema.set('toJSON', {
     transform: (document, returnedObject) => {
-      returnedObject.id = returnedObject._id.toString()
-      delete returnedObject._id
-      delete returnedObject.__v
+        returnedObject.id = returnedObject._id.toString()
+        delete returnedObject._id
+        delete returnedObject.__v
     }
-  })
-  
-  module.exports = mongoose.model('Person', personSchema)
+})
+
+module.exports = mongoose.model('Person', personSchema)
